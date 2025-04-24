27ff32a13c86c22dc33ddb6ea2e0e20e345be0f8
cariotan <a@cario.me>
2025-04-24 19:30:26 +1000

    Added UserManager and SignInManager to constructor

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,7 +1,8 @@
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
-public class AccountController(ILogger<AccountController> logger) : HtmxController
+public class AccountController(ILogger<AccountController> logger, UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager) : HtmxController
 {
 	[AllowAnonymous]
 	[HttpGet]
