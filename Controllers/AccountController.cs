a8ca942986f794cae50808d21b9a122833788b51
cariotan <a@cario.me>
2025-05-11 12:32:16 +1000

    Using full variable names

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -2,13 +2,13 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
-public class AccountController(ILogger<AccountController> l, UserManager<ApplicationUser> u, SignInManager<ApplicationUser> s) : HtmxController
+public class AccountController(ILogger<AccountController> logger, UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager) : HtmxController
 {
 	[AllowAnonymous]
 	[HttpGet]
 	public IActionResult Login()
 	{
-		l.Endpoint(Get, "/Account/Login");
+		logger.Endpoint(Get, "/Account/Login");
 
 		return View();
 	}
