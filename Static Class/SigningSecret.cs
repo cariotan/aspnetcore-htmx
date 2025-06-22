8e8f6a1a06f77d74a4e49c532e8abdbe4e944081
cariotan <a@cario.me>
2025-06-22 12:22:02 +1000

    Added .env file

--- Static Class/SigningSecret.cs
+++ Static Class/SigningSecret.cs
@@ -1,4 +1,4 @@
 static partial class StaticMethods
 {
-	public static string SigningSecret => Environment.GetEnvironmentVariable("JwtSecret", EnvironmentVariableTarget.Machine) ?? throw new Exception("Jwt signing secret not set");
+	public static string SigningSecret => Environment.GetEnvironmentVariable("JwtSecret") ?? throw new Exception("Jwt signing secret not set");
 }
