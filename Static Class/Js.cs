ac80f305c1f0b97f1cf67fd7bf7bd45f9558e9df
cariotan <a@cario.me>
2025-06-26 21:13:43 +1000

    Update Js.cs

--- Static Class/Js.cs
+++ Static Class/Js.cs
@@ -1,9 +1,14 @@
+using System.Text.Encodings.Web;
 using System.Text.Json;
 
 static partial class StaticMethods
 {
 	public static string Js(object @object)
 	{
-		return JsonSerializer.Serialize(@object);
+		return JsonSerializer.Serialize(@object, new JsonSerializerOptions
+		{
+			WriteIndented = true,
+			Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+		});
 	}
 }
