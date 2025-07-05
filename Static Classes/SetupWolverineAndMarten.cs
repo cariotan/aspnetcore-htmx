c7b1c14945c9ec004ed81d1f86807e297be50c61
cariotan <a@cario.me>
2025-07-06 02:25:12 +1000

    .

--- Static Classes/SetupWolverineAndMarten.cs
+++ Static Classes/SetupWolverineAndMarten.cs
@@ -10,15 +10,15 @@ static partial class StaticMethods
 			x.Policies.AutoApplyTransactions();
 		});
 
-		builder.Services.AddMarten(x =>
-		{
-			x.Connection("Host=localhost;Port=5432;Database=;Username=;Password=");
-			x.UseSystemTextJsonForSerialization();
-			if (builder.Environment.IsDevelopment())
-			{
-				x.AutoCreateSchemaObjects = AutoCreate.All;
-				x.DisableNpgsqlLogging = true;
-			}
-		}).IntegrateWithWolverine();
+		// builder.Services.AddMarten(x =>
+		// {
+		// 	x.Connection("Host=localhost;Port=5432;Database=;Username=;Password=");
+		// 	x.UseSystemTextJsonForSerialization();
+		// 	if (builder.Environment.IsDevelopment())
+		// 	{
+		// 		x.AutoCreateSchemaObjects = AutoCreate.All;
+		// 		x.DisableNpgsqlLogging = true;
+		// 	}
+		// }).IntegrateWithWolverine();
 	}
 }
