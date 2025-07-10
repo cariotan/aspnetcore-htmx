8b0006b63837d64f91927dc809b91caa642771ce
cariotan <a@cario.me>
2025-07-11 09:56:16 +1000

    Added Tell

--- Static Classes/Js.cs
+++ Static Classes/Js.cs
@@ -3,12 +3,14 @@ using System.Text.Json;
 
 static partial class StaticMethods
 {
+	private static JsonSerializerOptions options = new()
+	{
+		WriteIndented = true,
+		Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+	};
+
 	public static void Js(object @object)
 	{
-		Console.WriteLine(JsonSerializer.Serialize(@object, new JsonSerializerOptions
-		{
-			WriteIndented = true,
-			Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
-		}));
+		Console.WriteLine(JsonSerializer.Serialize(@object, options));
 	}
 }
