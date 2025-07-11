2557112652751c1794180c6a6fcfb55ade6fc55b
cariotan <a@cario.me>
2025-07-11 22:14:43 +1000

    Made Database path cross platform

--- Static Classes/DatabasePath.cs
+++ Static Classes/DatabasePath.cs
@@ -1,4 +1,20 @@
+using System;
+using System.IO;
+
 static partial class StaticMethods
 {
-	public const string DatabasePath = "C:\\Database";
+    public static readonly string DatabasePath;
+
+    static StaticMethods()
+    {
+        if (OperatingSystem.IsWindows())
+        {
+            DatabasePath = @"C:\Database\ccTimerNet";
+        }
+        else
+        {
+            var home = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            DatabasePath = Path.Combine(home, "Database", "ccTimerNet");
+        }
+    }
 }
