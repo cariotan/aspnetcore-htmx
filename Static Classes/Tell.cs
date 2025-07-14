6e467f67c40c74e8ac2f11eb700142916d625a6f
cariotan <a@cario.me>
2025-07-14 10:00:15 +1000

    Gave a default timeout from brain.Ask

--- Static Classes/Tell.cs
+++ Static Classes/Tell.cs
@@ -7,6 +7,6 @@ static partial class StaticMethods
 
 	public static async Task<T> Ask<T>(this IRequiredActor<Brain> actorRef, object message, TimeSpan? timeout = null)
 	{
-		return await actorRef.ActorRef.Ask<T>(message, timeout);
+		return await actorRef.ActorRef.Ask<T>(message, timeout ?? TimeSpan.FromSeconds(5));
 	}
 }
