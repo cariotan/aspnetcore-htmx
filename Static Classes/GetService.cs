b724427008ba60726d8bf5dbd3812ac7adec5775
cariotan <a@cario.me>
2025-07-17 11:17:24 +1000

    Delete GetService.cs

--- Static Classes/GetService.cs
+++
@@ -1,8 +0,0 @@
-static partial class StaticMethods
-{
-	public static T GetService<T>(this IServiceScope serviceScope)
-	{
-		var service = serviceScope.ServiceProvider.GetRequiredService<T>();
-		return service;
-	}
-}
