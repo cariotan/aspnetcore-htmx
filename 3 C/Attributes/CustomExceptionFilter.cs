1dbaa01ea0fd8d09459ea0556b3146f82bb3591c
cariotan <a@cario.me>
2025-07-20 00:21:42 +1000

    Update CustomExceptionFilter.cs

--- 3 C/Attributes/CustomExceptionFilter.cs
+++ 3 C/Attributes/CustomExceptionFilter.cs
@@ -4,7 +4,7 @@ public class CustomExceptionFilter(ILogger<CustomExceptionFilter> logger, IRequi
 {
 	public void OnException(ExceptionContext context)
 	{
-		logger.LogError(context.Exception, "Unhandled Halo Dashboard exception occurred");
+		logger.LogError(context.Exception, "Unhandled exception occurred");
 
 		brain.Tell(new SendEmailException(context.Exception));
 
