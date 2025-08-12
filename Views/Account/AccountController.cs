82a947709d1c3a0ca1c23358244ce9d9f21ce322
cariotan <a@cario.me>
2025-08-13 01:55:06 +1000

    Removed useless IHasUser from AccountController

--- Views/Account/AccountController.cs
+++ Views/Account/AccountController.cs
@@ -3,10 +3,8 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
-public class AccountController(ILogger<AccountController> logger, UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager) : HtmxController, IHasUser
+public class AccountController(ILogger<AccountController> logger, UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager) : HtmxController
 {
-	public ApplicationUser CurrentUser { get; set; } = null!;
-
 	[AllowAnonymous]
 	[HttpGet]
 	public IActionResult Register()
