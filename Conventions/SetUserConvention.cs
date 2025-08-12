35f6b960b63802ea97b01860318dcd8b82302ea0
cariotan <a@cario.me>
2025-08-13 01:54:02 +1000

    Removed dangerous async void

--- Conventions/SetUserConvention.cs
+++ Conventions/SetUserConvention.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ApplicationModels;
 using Microsoft.AspNetCore.Mvc.Filters;
 
@@ -16,20 +17,29 @@ public class SetUserConvention : IApplicationModelConvention
 	}
 }
 
-public class SetUserAttribute : ActionFilterAttribute
+public class SetUserAttribute : IAsyncActionFilter
 {
-	public override async void OnActionExecuting(ActionExecutingContext context)
+	public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
 	{
 		if (context.Controller is IHasUser controller)
 		{
 			var userManager = context.HttpContext.RequestServices.GetService<UserManager<ApplicationUser>>();
-
 			var user = await userManager!.GetUserAsync(context.HttpContext.User);
 
 			if (user is { })
 			{
 				controller.CurrentUser = user;
+				await next();
 			}
+			else
+			{
+				context.Result = new UnauthorizedResult();
+				return;
+			}
+		}
+		else
+		{
+			await next();
 		}
 	}
 }
