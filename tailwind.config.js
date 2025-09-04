41201d877257abc4167f8753c335a1111799efb6
cariotan <a@cario.me>
2025-09-04 10:37:06 +1000

    Moved theme from tailwind.config to its own file to share with the tailwind cdn

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,8 @@
+import { theme } from './wwwroot/js/theme.js'
+
 module.exports = {
 	content: ['Controllers/*.cs', 'Views/**/*.{cshtml,html,cs}', 'wwwroot/**/*.{css,html,js}', 'Static Classes/*.cs'],
-	theme: {
-		extend: {
-			colors: {
-			},
-			screens: {
-			},
-		},
-		fontFamily: {
-		},
-	},
+	theme: theme,
 	plugins: [
 		require('@tailwindcss/forms')({
 			strategy: 'class',
