31eae6a31fb9efdb0936a5641f37b141b76c008a
cariotan <a@cario.me>
2025-09-11 18:01:23 +1000

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 import { theme } from './wwwroot/js/theme.js'
 
 module.exports = {
-	content: ['Controllers/*.cs', 'Views/**/*.{cshtml,html,cs}', 'wwwroot/**/*.{css,html,js}', 'Static Classes/*.cs'],
+	content: ['Controllers/*.cs', 'Views/**/*.{cshtml,html,cs}', 'wwwroot/**/*.{css,html,js}', 'Static Classes/*.cs', 'Areas/**/*.{cshtml,html}'],
 	theme: theme,
 	plugins: [
 		require('@tailwindcss/forms')({
