eadc1b184f6c0a5550eee36e19a06b46f740a94e
cariotan <a@cario.me>
2025-10-03 09:31:52 +1000

    Cleaned up content in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 import { theme } from './wwwroot/js/theme.js'
 
 module.exports = {
-	content: ['Controllers/*.cs', 'Views/**/*.{cshtml,html,cs}', 'wwwroot/**/*.{css,html,js}', 'Static Classes/*.cs', 'Areas/**/*.{cshtml,html}'],
+	content: [
+		'Controllers/*.cs',
+		'Views/**/*.{cshtml,html,cs}',
+		'wwwroot/**/*.{html,js}',
+		'Static Class/*.cs',
+		'Areas/**/*.{cshtml,html}',
+		'!wwwroot/js/alpine.js',
+		'!wwwroot/js/htmx.js',
+		'!wwwroot/js/signalr/dist/browser/signalr.js',
+	],
 	theme: theme,
 	plugins: [
 		require('@tailwindcss/forms')({
