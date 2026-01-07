2dfe065757f5efc58c6b3795b51354f7e8b27417
cariotan <a@cario.me>
2026-01-07 14:01:12 +1100

    Changed JWT token expiry to 10 minutes

--- Static Class/GenerateJwtToken.cs
+++ Static Class/GenerateJwtToken.cs
@@ -15,7 +15,7 @@ static partial class StaticMethods
 
 		return new JwtSecurityTokenHandler().WriteToken(new JwtSecurityToken(
 			claims: claims,
-			expires: DateTime.Now.AddSeconds(10),
+			expires: DateTime.Now.AddHours(1),
 			signingCredentials: new(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(SigningSecret ?? throw new Exception("SigningSecret is null."))), SecurityAlgorithms.HmacSha256),
 			issuer: "cario",
 			audience: "cario"
